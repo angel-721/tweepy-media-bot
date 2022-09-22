ce87e1ebc26de91c9c77cb0e32f8aff7384d0793
angel <angel@pop-os.localdomain>
2022-09-22 15:11:47 -0600

    added bot loop and updated filenames script

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from pyfiles.botmethods import Bot
+from time import sleep
 
+#1) USE THIS IF USING A CRONJOB TO DEPLOY:
 bot = Bot()
 bot.makeTweet()
+
+#2 USE THIS IF USING A CRONJOB TO DEPLOY:
+#bot = Bot()
+#running = True
+#while running:
+#    bot.makeTweet()
+#    ##wait N many seconds. 3600 seconds = 1 hour
+#    sleep(3600)
