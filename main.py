6614cb949e2baecf9f3c32ddb9411b800cd8522f
angel-721 <angelvelasquez2002@gmail.com>
2023-06-14 17:02:45 -0600

    alter main

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python
-import argparse
-from time import sleep
-from pyfiles.botmethods import Bot
-from pyfiles.database import populateTable
-from os import system
-
 """
-Make sure if you don't have a ./pyfiles/image.db file run the make-database
+Main file that calls the helper methods to delpy the bot.
+
+Make sure if you don't have a ./pyfiles/image.db file run the make-database.
 command before trying to tweet!
 """
+import argparse
+from time import sleep
+
+from pyfiles.botmethods import Bot
+from pyfiles.database import populateTable
 
 bot = Bot()
 
 # 1) USE THIS IF USING A CRONJOB TO DEPLOY:
+
+
 def cronJobDeploy():
     bot.makeTweet()
 
+
 # 2 USE THIS IF USING A INFINTE LOOP TO DEPLOY:
+
+
 def loopDeploy():
     running = True
     while running:
@@ -24,25 +30,28 @@ def loopDeploy():
         # 3600 seconds = 1 hour
         sleep(3600)
 
-"""
 
-Function to parse cli arguments mainly for getting the type of deployment
-
-"""
 def parseArgs():
-    parser = argparse.ArgumentParser(prog="Tweepy-Media-Bot", description="Tweets and Likes Media in a automated fasion")
-    parser.add_argument("-deploy", choices=("cronjob", "loop"))
-    parser.add_argument("-make-database", type=int, default=0)
+    """Parse cli arguments mainly for getting the type of deployment."""
+    parser = argparse.ArgumentParser(
+        prog='Tweepy-Media-Bot',
+        description='Tweets and Likes Media in a automated fasion',
+    )
+    parser.add_argument('-deploy', choices=('cronjob', 'loop'))
+    parser.add_argument('-make-database', type=int, default=0)
     args = parser.parse_args()
     return args
 
+
 def main(args):
-    if (args.deploy == "cronjob"):
+    """Handle the command line args and call functions."""
+    if args.deploy == 'cronjob':
         cronJobDeploy()
-    elif (args.deploy == "loop"):
+    elif args.deploy == 'loop':
         loopDeploy()
-    if (args.make_database != 0):
+    if args.make_database != 0:
         populateTable()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main(parseArgs())
